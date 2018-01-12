5c406f9bb43f7fc6422685eca4d2a16ad5b035e1
hskEvelin <lucas.greising@hs-kempten.de>
2018-01-12 12:43:28 +0100

    created Service for parceloptions

---
+++ src/service/Address.java
@@ -0,0 +1,41 @@
+package service;
+
+public class Address {
+	private String name;
+	private String firstname;
+	private String street;
+	private int code;
+	private String city;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
---
+++ src/service/PickupOption.java
@@ -0,0 +1,9 @@
+package service;
+
+import java.util.Date;
+
+public class PickupOption {
+	private boolean parcelshop;
+	private Date date;
+	private Address address;
+}
